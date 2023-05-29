26422a16b0e60137d4aa82cdacc41b41a6c40095
Anieb22 <anielloverdino@gmail.com>
2023-05-29 23:06:58 +0200

    Eseguito bonus con select

--- js/javascript.js
+++ js/javascript.js
@@ -3,10 +3,33 @@ let play = document.getElementById('play');
 
 
 play.addEventListener('click', function(){
+    const difficulty = parseInt(document.getElementById('difficulty').value);
+
+    let cellNumber;
+
+    switch(difficulty){
+        case 1:
+            cellNumber = 100;
+            break;
+        
+        case 2:
+            cellNumber = 81;
+            break;
+        case 3:
+            cellNumber = 49;
+            break;
+    }
+
+    console.log(cellNumber)
+
     output.innerHTML = '';
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= cellNumber; i++) {
         let elem = document.createElement('div');
+        let cellPerRow = Math.sqrt(cellNumber)
+        console.log(cellPerRow)
         elem.classList.add('square');
+        elem.style.width = `calc(90% / ${cellPerRow})`;
+        elem.style.width = elem.style.width;
         elem.innerText = i;
         output.append(elem);
     }
@@ -15,7 +38,7 @@ let squares = document.querySelectorAll('.square');
 
 squares.forEach(function(square) {
     square.addEventListener('click', function() {
-        this.classList.toggle('active');
+        this.classList.add('active');
         console.log(this.innerText);
     });
 });
